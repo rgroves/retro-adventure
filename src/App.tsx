861ed9c624ae32f80b1daa176d5ede98ffa3a938
Robert Groves <rwgdev@gmail.com>
2024-07-24 10:38:56 -0500

    Remove template artifacts

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import { Amplify } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react'
-import { useState } from 'react'
 import '@aws-amplify/ui-react/styles.css';
 import outputs from '../amplify_outputs.json';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 Amplify.configure(outputs)
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <Authenticator>
@@ -18,25 +14,7 @@ function App() {
         <>
           <div>
             <h1>Hello {user?.username}</h1>
-            <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-              <img src={viteLogo} className="logo" alt="Vite logo" />
-            </a>
-            <a href="https://react.dev" target="_blank" rel="noreferrer">
-              <img src={reactLogo} className="logo react" alt="React logo" />
-            </a>
           </div>
-          <h1>Vite + React</h1>
-          <div className="card">
-            <button onClick={() => { setCount((count) => count + 1) }}>
-              count is {count}
-            </button>
-            <p>
-              Edit <code>src/App.tsx</code> and save to test HMR
-            </p>
-          </div>
-          <p className="read-the-docs">
-            Click on the Vite and React logos to learn more
-          </p>
           <button onClick={signOut}>Sign out</button>
         </>
       )}
