c3082d529b88192d5620704899c8ba34fce55600
Robert Groves <rwgdev@gmail.com>
2024-07-27 22:46:50 -0500

    Clean up user menu

--- src/components/UserMenu.tsx
+++ src/components/UserMenu.tsx
@@ -1,30 +1,33 @@
-import { useEffect, useState } from 'react';
-import { Flex, Loader, useTheme, Text, Button } from '@aws-amplify/ui-react';
-import { useAuthenticator } from '@aws-amplify/ui-react'
-import { fetchUserAttributes } from 'aws-amplify/auth';
+import { useEffect, useState } from "react";
+import { fetchUserAttributes } from "aws-amplify/auth";
+import {
+  Flex,
+  Loader,
+  useTheme,
+  Text,
+  Button,
+  useAuthenticator,
+} from "@aws-amplify/ui-react";
 
 export default function UserMenu() {
-    const { user, signOut } = useAuthenticator((context) => [context.user]);
-    let [userNick, setUserNick] = useState("");
+  const { user, signOut } = useAuthenticator((context) => [context.user]);
+  const [userNickname, setUserNickname] = useState("");
+  const { tokens } = useTheme();
 
-    useEffect(() => {
-        async function loadUserAttributes() {
-            const attributes = await fetchUserAttributes();
-            setUserNick(attributes.nickname || "");
-        }
-        loadUserAttributes();
-    }, [user])
+  useEffect(() => {
+    async function loadUserAttributes() {
+      const attributes = await fetchUserAttributes();
+      setUserNickname(attributes.nickname || "");
+    }
+    loadUserAttributes();
+  }, [user]);
 
-    const { tokens } = useTheme();
-    const nickname = userNick ? <Text>Welcome {userNick}</Text> : <Loader />;
-    return (
-        <Flex
-            color={tokens.colors.font.primary}
-            justifyContent="flex-end"
-            alignItems="center"
-            gap={tokens.space.small}>
-            {nickname}
-            <Button size="small" onClick={signOut}>Sign out</Button>
-        </Flex>
-    );
+  return (
+    <Flex justifyContent="flex-end" alignItems="center" gap={tokens.space.xl}>
+      {userNickname ? <Text>Welcome {userNickname}</Text> : <Loader />}
+      <Button size="small" onClick={signOut}>
+        Sign out
+      </Button>
+    </Flex>
+  );
 }
